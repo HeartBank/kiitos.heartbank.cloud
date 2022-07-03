a497fc55646459efc729d51294b28919a096d0a9
Thon Ly <thonly@heartbank.ceo>
2022-07-03 15:19:45 -0700

    test cors

--- public/index.js
+++ public/index.js
@@ -36,3 +36,8 @@ async function testLogin3() {
       
     return response.json();
 }
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-B5Z3BX0LMJ');
